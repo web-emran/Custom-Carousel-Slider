104f786397b1bafbf2b14bd22741fb2005012de1
fed.emran <fed.emran@gmail.com>
2022-05-19 16:00:01 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Custom Carousel Slider
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/emrankhan016/pen/zYGbOma](https://codepen.io/emrankhan016/pen/zYGbOma).
+
---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+			$('.carousel').carousel({
+				carouselWidth:930,
+				carouselHeight:330,
+				directionNav:true,
+				shadow:true,
+				buttonNav:'bullets'
+			});
+		});
